2edaabd3825e7d8b40e19e0c44c0723e6e6dceda
josethec <josethec@gmail.com>
2020-03-17 18:57:59 -0600

    script 20200317-2

--- cte.sql
+++ cte.sql
@@ -17,3 +17,11 @@ with fecha_duplicada as (
 	group by payment_date
 ) 
 select * from fecha_duplicada where fecha_duplicada.grp>1
+
+with cte_staff as (
+select staff_id,count(rental_id) as conteo
+from rental
+group by staff_id
+	)
+select s.staff_id,s.first_name,s.last_name, cs.conteo from staff s
+inner join cte_staff as cs on (s.staff_id=cs.staff_id)
